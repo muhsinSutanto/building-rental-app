915dd9b2a83728d82525491714822677bab0389d
muhsin sutanto <muhsin@femaledaily.com>
2020-05-20 01:19:02 +0700

    Updates

---
+++ precache-manifest.b9fb2ba66d173efec172c8199a6c8f71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "941c88f7dd85f4f641de56dc8a08d845",
+    "url": "/building-rental-app/index.html"
+  },
+  {
+    "revision": "1f290c0c01f6e6c3db10",
+    "url": "/building-rental-app/static/css/main.e156909f.chunk.css"
+  },
+  {
+    "revision": "988eab41403c2f1d2d39",
+    "url": "/building-rental-app/static/js/2.fa509445.chunk.js"
+  },
+  {
+    "revision": "6791e7fb9584d65ed87d655035c7da95",
+    "url": "/building-rental-app/static/js/2.fa509445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f290c0c01f6e6c3db10",
+    "url": "/building-rental-app/static/js/main.2cdb8805.chunk.js"
+  },
+  {
+    "revision": "3f63e69e0f967adec2dd",
+    "url": "/building-rental-app/static/js/runtime-main.5c7ab1d9.js"
+  }
+]);
